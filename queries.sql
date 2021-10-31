23fe5e9dd09627c13e95be27caf8c2efda416546
Terrel <TerrelRJones@gmail.com>
2021-10-30 23:44:57 -0700

    assignment 4 done

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
 ## Part 1: Test it with SQL
 
+
 ## Part 2: Test it with SQL
+SELECT * FROM employer WHERE location = "Seattle, Wa";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+
+SELECT name, description FROM job_skills JOIN skill ON job_skills.skills_id = skill.id WHERE jobs_id IS NOT NULL;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,43 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
+
+    public Skill(String description, List<Job> jobs) {
+        this.description = description;
+        this.jobs = jobs;
+    }
+
+    public Skill(){
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
